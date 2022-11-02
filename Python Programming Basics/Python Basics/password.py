b87edfebf3cda57bf0df0174514bb3d6fafd7d08
sproods <106177602+sproods@users.noreply.github.com>
2022-11-01 23:29:56 -0500

    Primera parte del ejercicio para entender la declaración while.

---
+++ Python Programming Basics/Python Basics/password.py
@@ -0,0 +1,10 @@
+password = 'magicWord'
+
+while True:
+    print('Digite su contraseña:')
+    yourPassword = input()
+
+    if yourPassword == password:
+        break
+
+print('\nAcceso concedido\n')
