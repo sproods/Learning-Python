99b916fc1c9fde8c6d5ae6c9bf74309cb4e488c4
sproods <106177602+sproods@users.noreply.github.com>
2022-11-01 23:46:52 -0500

    Actualización del ejercicio.

--- Python Programming Basics/Python Basics/password.py
+++ Python Programming Basics/Python Basics/password.py
@@ -1,10 +1,20 @@
 password = 'magicWord'
+a = 0
 
 while True:
     print('Digite su contraseña:')
     yourPassword = input()
 
-    if yourPassword == password:
+    if yourPassword != password:
+        a += 1
+    else:
         break
 
-print('\nAcceso concedido\n')
+    if a == 5:
+        break
+
+
+if a < 5:
+    print('\nAcceso concedido')
+elif a == 5:
+    print('\nAcceso denegado')
