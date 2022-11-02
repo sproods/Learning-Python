f14d4061cd8acc8bf6c2cb448df5d6735e03825a
sproods <106177602+sproods@users.noreply.github.com>
2022-11-01 23:55:40 -0500

    Ejercicio de repaso instintivo sobre la declaración 'for'

---
+++ Python Programming Basics/Python Basics/loopFor.py
@@ -0,0 +1,7 @@
+# Breve prueba instintiva de cómo funciona la declaración for.
+
+m = input('Digite desde qué número desea contar: ')
+n = input('Digite hasta qué número quiere contar (eseptuando este número: ')
+
+for i in range(int(m), int(n)):
+    print(i)
