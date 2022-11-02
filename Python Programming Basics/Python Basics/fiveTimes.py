353f96c19784461d5b0f135d1fb4de12ed6157e4
sproods <106177602+sproods@users.noreply.github.com>
2022-11-02 18:43:05 -0500

    Programa alternativo de un bucle usando un while statement.

--- Python Programming Basics/Python Basics/fiveTimes.py
+++ Python Programming Basics/Python Basics/fiveTimes.py
@@ -1,3 +1,8 @@
 print('My name is')
-for i in range(5):
+# for i in range(5):
+#     print('Jimmy Five Times (' + str(i) + ')')
+
+i = 0
+while i < 5:
     print('Jimmy Five Times (' + str(i) + ')')
+    i += 1
