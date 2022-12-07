3c15de5c306345923dfc17318033e361b727c64c
sproods <106177602+sproods@users.noreply.github.com>
2022-12-07 16:58:01 -0500

    mejoramiento del programa comma_code.py

--- Python Programming Basics/Lists/practice_projects/comma_code.py
+++ Python Programming Basics/Lists/practice_projects/comma_code.py
@@ -1,6 +1,16 @@
 # Proyecto de práctica.
 # Programa que muestra en pantalla el contenido de una lista desplegada entre comas y la palabra 'and' para denotar la última palabra.
 
+# función que mostrará en pantalla la lista
+def catalogo(list_name):
+    tam = len(list_name)
+    
+    for i in range(tam - 2):
+        print(list_name[i], end=', ')
+    
+    print(list_name[tam - 2] + ' and ' + list_name[tam - 1])
+    
+# Programa principal
 lista = []          # lista vacía
 
 i = 0
@@ -13,16 +23,6 @@ while True:
         i += 1
     else:
         break
-
-# función que mostrará en pantalla la lista
-def catalogo(list_name):
-    tam = len(list_name)
-    
-    for i in range(tam - 1):
-        print(list_name[i], end=', ')
-    
-    print('and')
-    print(list_name[-1])
     
 # llamada a la función catalogo
 catalogo(lista)
