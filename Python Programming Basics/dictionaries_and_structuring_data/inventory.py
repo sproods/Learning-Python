bb322f6e4059cb7b467a17937222a01d7deac2ad
sproods <106177602+sproods@users.noreply.github.com>
2022-12-27 12:54:50 -0500

    inplementación de la función que añade nuesvos items y valores al inventario.

--- Python Programming Basics/dictionaries_and_structuring_data/inventory.py
+++ Python Programming Basics/dictionaries_and_structuring_data/inventory.py
@@ -8,15 +8,36 @@ def displayInventory(inventory):
         item_total += v
     print("Total number of items: " + str(item_total))
 
-# main
-print('Digite la opción de su preferencia:')
-print('1. Añadir nuevos keys')
-print('2. Aumentar los valores de keys existentes')
-print('3. Disminuir los valores de keys existentes')
-print('4. Ver el inventario total')
-op = input()
+# Función que realiza la adición de nuevos keys, o componentes, pero si ese componente existe modifica el valor
+def newKey(inventory):
+    print('Digite el nuevo key:')
+    key = input()
+    print('Ahora, digite el nuevo valor para ' + key + ':')
+    value = input()
+    inventory.setdefault(key, int(value))
 
-if str(op) == '1':
-    print(op)
-elif str(op) == '4':
-    displayInventory(stuff)
+# main
+while True:
+    print('Digite la opción de su preferencia:')
+    print('1. Añadir nuevos keys')
+    print('2. Aumentar los valores de keys existentes')
+    print('3. Disminuir los valores de keys existentes')
+    print('4. Ver el inventario total')
+    op = input()
+
+    if int(op) == 1:
+        newKey(stuff)
+    elif int(op) == 2:
+        addValues(stuff)
+    elif int(op) == 3:
+        quitValues(stuff)
+    elif int(op) == 4:
+        displayInventory(stuff)
+
+    print('Desea digitar otra opción? (s/n):')
+    op2 = input()
+
+    if op2 == 'n':
+        print('Gracias por sus consultas!')
+        break
+
