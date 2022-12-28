99bd181170f6c1554a31b6d5147071d211736f7f
sproods <106177602+sproods@users.noreply.github.com>
2022-12-28 10:16:04 -0500

    programa para entender la manipulación de los strings y los escapes de línea así como los comentarios de multiplelínea.

---
+++ Python Programming Basics/manipulating_strings/catnapping.py
@@ -0,0 +1,19 @@
+'''Los comentarios se multilínea se pueden hacer de esta manera.
+Averiguemos śobre cómo mejorar los conocimientos en python.'''
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Bob''')
+
+print('\n')
+
+"""Como puede verse, los comentarios multilínea
+funcionan tanto con las triples comillas simples
+como con las triples comillas dobles."""
+"""Como puede verse, los comentarios multilínea
+funcionan tanto con las triples comillas simples
+como con las triples comillas dobles."""
+
+print('Dear Alice,\n\nEve\'s cat has been arrested for catnapping, cat burglary, and extortion.\n\nSincerely,\nBob')
